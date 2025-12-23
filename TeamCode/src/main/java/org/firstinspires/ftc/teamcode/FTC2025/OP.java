47c0c7c11becbb8439be4216a70ccd5ae8323ae0
dashboard <yaoqing1124@gmail.com>
2025-12-23 16:22:49 +0800

    123

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2025/OP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2025/OP.java
@@ -10,25 +10,9 @@ public class OP extends OpMode {
     @Override
     public void init() {
         robot = new RobotBase(hardwareMap);
-//        robot.spinPosition(60);
-        robot.spinPower(0);
-        robot.armout();
     }
 
     @Override
     public void loop() {
-//        robot.ApriltagAim();
-//        robot.ShooterAim();
-        robot.move(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
-        robot.shooter(gamepad1.right_trigger);
-        if(gamepad1.right_bumper) robot.armin();
-        if(gamepad1.left_bumper)robot.armout();
-        robot.spinPower(0.3);
-//        robot.spincolor();
-        robot.riseball();
-        robot.intake(1);
-//        telemetry.addData("color1",robot.getColor1());
-//        telemetry.addData("color1",robot.getColor2());
-//        telemetry.addData("color1",robot.getColor3());
     }
 }
